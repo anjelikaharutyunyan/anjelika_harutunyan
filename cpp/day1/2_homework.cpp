8d694c89051f671f94db7f661f09489276948674
Angelika <you@example.com>
2022-03-03 11:26:13 -0500

    added homework file

---
+++ cpp/day1/2_homework.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  int x,i = 1;
+	cout << "mutqagrel tivy ";
+	cin >> x;
+	while(i <= 10){
+	cout << x << "*" << i << "=" << x*i <<endl;
+	i++;
+}
+  return 0;
+}
+
+
+
+
+
