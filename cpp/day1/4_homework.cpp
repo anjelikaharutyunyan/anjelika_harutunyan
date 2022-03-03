f1d948bff3a796fa0e692b749f96f01732f85730
Angelika <you@example.com>
2022-03-03 13:55:16 -0500

    4th homework added

---
+++ cpp/day1/4_homework.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	 int chickens, cows, pigs;
+	 int sum=0;
+	cout << " Chickens are ";
+	cin >> chickens;
+	cout << "Cows are ";
+	cin >> cows;
+	cout << "Pigs are ";
+	 cin >> pigs;
+sum = 2*chickens + 4*cows + 4*pigs;
+
+cout << "Farmer has " << sum << " animals in his farm";
+
+ 	
+
+
+return 0;
+}
