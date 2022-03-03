3a66c2ae803f6c2b711978111951a308f04e1833
Angelika <you@example.com>
2022-03-03 13:21:20 -0500

    added 5th homework

---
+++ cpp/day1/5_homework.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+int edg, edg2;
+	cout << "Enter the 1st edg of rectangle ";
+	cin >> edg;
+	cout << "Enter the 2nd edg of rectangle ";
+	cin >> edg2;
+	 cout << "Area of Rectangle = " << edg*edg2 << endl;
+	cout << "Perimeter of Rectangle = " << (edg+edg2)*2;
+
+
+return 0;
+}
