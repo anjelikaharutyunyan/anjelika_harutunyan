38700833d9c2ae25cda0d6fdc991bb7d88cd2f0c
Angelika <you@example.com>
2022-03-04 12:53:56 -0500

    added corrected hometask

--- cpp/day1/1_homework.cpp
+++ cpp/day1/1_homework.cpp
@@ -2,24 +2,19 @@
 using namespace std;
 
 int main(){
-	int x,a;
-	cout << "mtapahel tiv ";
-	cin >> a;
-
-while ( x < 0 || x > 100){
-	cout << "mutqagrel mtapahvac tivy";
-	cin >> x;
-}
+        int x, a;
+        cout << "mtapahel tiv ";
+        cin >> a;
 
 do {
- 	cout << "mutqagrel mtapahvac tivy ";
-	cin >> x;
-if ( x > a)		cout << "tivy mec e mtapahvac tvic, pordzel krkin";
-else if ( x < a) 	cout << "tivy poqr e mtapahvac tvic, pordzel krknin";
-else if( x==a)		 cout << "duq jisht eq gushakel tivy";
+        cout << "mutqagrel mtapahvac tivy ";
+        cin >> x;
+if ( x > a )            cout << "tivy mec e mtapahvac tvic, pordzel krkin";
+else if ( x < a )       cout << "tivy poqr e mtapahvac tvic, pordzel krknin";
+else if( x==a )          cout << "duq jisht eq gushakel tivy";
 }
 
-while( x!=a);
+while( x!=a );
 
 return 0;
 }
