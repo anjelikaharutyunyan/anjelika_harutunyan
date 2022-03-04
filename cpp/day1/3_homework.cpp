e79d2753565c3a88566bcfc2e5eb030a761d8ef1
Angelika <you@example.com>
2022-03-04 12:57:29 -0500

    added corrected homework

--- cpp/day1/3_homework.cpp
+++ cpp/day1/3_homework.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
-#include <math.h>
+
 using namespace std;
 
 int main()
 {
-int year,count = 0;
-	cout << "mutqagrel taretivy ";
-	cin >> year;
-while (count != 20){
-if ((year%400==0) || (year%4==0 && year%100 != 0)){
-	year = year+4;
-	cout << year << "is leap year "<<endl;
+int year, count=0;
+cin>>year;
+while ( count != 20 ){
+    
+if  (year % 4 == 0&& year % 100 != 0){
+         count = count + 1;
+        cout << year << "is leap year "<<endl;
 }
-	count = count + 1;
+        year++;
+       
 }
+
 return 0;
 }
 
