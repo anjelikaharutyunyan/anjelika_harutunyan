fef9965a31e634ebdbbf9d6831deccc0647fc458
Angelika <you@example.com>
2022-03-10 12:32:54 -0500

    binary

--- cpp/day4/1_binary_number.cpp
+++ cpp/day4/1_binary_number.cpp
@@ -3,17 +3,17 @@
     int main()  
     {  
     int a[1000], n, i;    
-    cout<<"Enter the number to convert: ";    
-    cin>>n;    
-    for(i=0; n>0; i++)    
+    cout << "Enter the number to convert: ";    
+    cin >> n;    
+    for( i=0; n>0; i++ )    
     {    
-    a[i]=n%2;    
-    n= n/2;  
+    a[i] = n%2;    
+    n = n/2;  
     }    
-    cout<<"Binary of the given number= ";    
-    for(i=i-1 ;i>=0 ;i--)    
+    cout << "Binary of the given number= ";    
+    for( i=i-1; i>=0; i--)    
     {    
-    cout<<a[i];    
+    cout << a[i];    
     }    
     }  
 
