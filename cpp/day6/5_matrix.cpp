92d57a6f5b0474d8e9206b1c5384294451a21491
Angelika <you@example.com>
2022-03-13 12:34:06 -0400

    Sum of two matrices

--- cpp/day6/5_matrix.cpp
+++ cpp/day6/5_matrix.cpp
@@ -3,17 +3,14 @@
 #include <cstdlib>
 using namespace std;
 int main(){
- int n, m, sum=0;
-	cout << "First matrix size: " << endl;
+ int n;
+ int sum=0;
+	cout << "Enter matrix size: " << endl;
 	cin >> n;
-	cout << "Second matrix size: " << endl;
-	cin >> m;
 int matrix1[n][n];
-int matrix2[m][m];
-if (n!=m){
-	cout << "The dimensions of these matrices don't  match";
- return 0; }
+int matrix2[n][n];
 
+cout << "First matrix" << endl;
  for(int i = 0; i < n; i++){
 	for (int j = 0; j < n; j++ ){
 	matrix1[i][j] = 1+ rand()%10;
@@ -22,15 +19,17 @@ if (n!=m){
 }
 	cout << endl; 
 }
-	cout << endl;
- for (int a = 0; a < m; a++){
-	for (int b = 0; b < m; b++){
+	cout << "Second matrix" << endl;
+	
+ for (int a = 0; a < n; a++){
+	for (int b = 0; b < n; b++){
 	matrix2[a][b] = 1 + rand()%10;
 	cout << matrix2[a][b] << "\t";
 }
 	cout << endl;
 }
-	cout << endl;
+
+	cout << "Sum of 2 matrices" << endl;
  for ( int i=0; i < n; i++){
 	for(int j = 0; j < n;j++){
 	  sum = matrix1[i][j] +  matrix2[i][j];
