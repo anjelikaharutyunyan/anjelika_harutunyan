1bcb11b1d4c1cf54f211bea8a9ca0b86432f1fcf
Angelika <you@example.com>
2022-03-22 16:12:35 -0400

    added unix time

---
+++ cpp/day11/unix.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+int main(){
+
+        long int second;
+        cout << "Enter seconds: ";
+        cin >> second;
+
+        time_t dt  = second;
+        string date = ctime(&dt);
+        string day = "";
+        string month = "";
+        string year = "";
+        string time = "";
+
+        for(int i = 0; date[i] != '\0'; i++){
+                if ( i >= 3 && i <= 6  ){
+                        day = day + date[i];
+ }
+          	  if( i >= 7 && i <= 9 ){
+                        month = month + date[i];
+}
+                if( i >= 10 && i <= 15){
+                        time = time + date[i];
+}
+                  if(i >= 17 && i <= 20){
+                        year = year + date[i];
+}
+}
+
+                cout << "Unix time: " << day + " " + month + " " + time + " " + year;
+
+
+
+        return 0;
+}
+
