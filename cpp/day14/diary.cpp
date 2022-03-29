679686efee564b59c934bfd6ca75f301c3237beb
Angelika <you@example.com>
2022-03-29 13:33:49 -0400

    added diary

--- cpp/day14/diary.cpp
+++ cpp/day14/diary.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 struct data{                          
@@ -10,7 +11,7 @@ struct data{
 };
 
 
-enum weekD{                          
+enum weekDay{                          
     Mon = 1,
     Tue,
     Wed,
@@ -21,8 +22,8 @@ enum weekD{
 };
 
  
-void  toStr(string, int*, int*, int*);
-int weekDay(int,int,int);
+void  adding_data_to_string(string, int*, int*, int*);
+int week(int, int, int);
 bool isEmpty(string);
 
 
@@ -31,19 +32,20 @@ bool isEmpty(string);
 int main() {   
     int dd = 0, mm  = 0, yy = 0; 
     struct data data_input;
+	
 
     while(true) {
 
       cout << "Input day (dd/mm/yy): ";
       cin >> data_input.dataInfo;
-	cin.ignore();
       cout << "Input Note: ";
+      cin.ignore();
       getline(cin,data_input.Note);
-      toStr(data_input.dataInfo,&dd,&mm, &yy);
+      adding_data_to_string(data_input.dataInfo,&dd,&mm, &yy);
       string dayname = "";   
    
-      int wday1 = weekDay(dd,mm,yy);
-      weekD wday=(weekD) wday1;
+      int wday1 = week(dd,mm,yy);
+      weekDay wday = (weekDay) wday1;
       switch(wday) {                          
           case Mon:  dayname = "Monday";    break;
           case Tue:  dayname = "Tuesday";   break;
@@ -54,80 +56,77 @@ int main() {
           case Sun:  dayname = "Sunday";    break;
       }
                  
-      string way = to_string(dd) + "_" + to_string(mm) + "_" + to_string(yy)+".txt";
-      fstream fis;
-      fis.open(way, fstream::in | fstream::out | fstream::app);
-      if (fis.is_open()) {
-	
-        if (isEmpty(way)) {
-            fis << dayname <<"\t" <<  data_input.dataInfo <<  "\t" <<  data_input.Note << "\n"; 
+      string file = to_string(dd) + "_" + to_string(mm) + "_" + to_string(yy)+".txt";
+      fstream myfile;
+      myfile.open(file, fstream::in | fstream::out | fstream::app);
+      if (myfile.is_open()) {	
+
+        if (isEmpty(file)) {
+            myfile << dayname <<"\t" <<  data_input.dataInfo <<  "\t" <<  data_input.Note << "\n"; 
           }
           else {
-            fis << "\t\t\t" <<  data_input.Note << "\n";
+            myfile << "\t\t\t" <<  data_input.Note << "\n";
           }  
       }
 
 cout << "Command: add/exit ";                                           
       cin >> data_input.command;
     
-      if(data_input.command == "add") {                                     
-          continue;
-      }
-      else if(data_input.command == "exit") {                               
+	if(data_input.command != "add" && data_input.command != "exit"){
+	
+	cout << "Enter command (add/exit): ";
+	cin >> data_input.command;
+
+	}
+}                        
           return 0;
       }
-      else {
-          cout << "Enter Command: add/exit: ";                             
-          cin >> data_input.command;
-      }
     
-    }
-
-    return 0;
-}
 
 
-
-
-
-
-
- int weekDay(int dd,int mm,int yy) {                     
-    int weeknum = dd + ((153 * (mm + 12 * ((14 - mm) / 12) - 3) + 2) / 5) 
+ int week(int dd, int mm, int yy) {                     
+    int weeknumber = dd + ((153 * (mm + 12 * ((14 - mm) / 12) - 3) + 2) / 5) 
                 + (365 * (yy + 4800 - ((14 - mm) / 12)))
                 + ((yy + 4800 - ((14 - mm) / 12)) / 4)
                 - ((yy + 4800 - ((14 - mm) / 12)) / 100)
                 + ((yy + 4800 - ((14 - mm) / 12)) / 400)
                 - 32045;
 
-    return (weeknum + 1) % 7 ;
+    return (weeknumber + 1) % 7 ;
 }
 
-void  toStr(string text,int *x, int *y, int* z) {        
+void  adding_data_to_string(string text, int *day, int *month, int *year) {        
     string  dx = "";
     string  dd = "";
     string  mm = "";
     string  yy = "";
     int temp = 0;
+
     for(int i = 0; text[i] !='\0'; i++) {
-      if(text[i] !='/') {
+
+      if(text[i] != '/') {
+
         dx += text[i];
+
         temp++;
       }
       else if((temp == 1 || temp == 2) && text[i] == '/') {
 
         if(dx[0] == '0' && temp == 2) {
-          dd =dx[1];
+
+          dd = dx[1];
+
           dx = " ";
         }
         else{
           dd = dx;
+
           dx = "";
         }
       }
       else if((temp >= 2 && temp <= 4) && text[i] == '/') {
         if(dx[0] == '0' && temp == 4) {
-          mm =dx[1];
+          mm = dx[1];
           dx = " ";
         }
         else {
@@ -138,19 +137,19 @@ void  toStr(string text,int *x, int *y, int* z) {
       }
     }
     yy = dx;
-    *x = stoi(dd);
-    *y = stoi(mm);
-    *z = stoi(yy);
+    *day = stoi(dd);
+    *month = stoi(mm);
+    *year = stoi(yy);
 }
 
-bool isEmpty(string way) {        
-    fstream fanc;
+bool isEmpty(string file) {        
+    fstream myfile;
     string str = "";
 
-    fanc.open(way, fstream::in | fstream::out | fstream::app);
+    myfile.open( file, fstream::in | fstream::out | fstream::app);
 
-    while (!fanc.eof()) {
-      fanc >> str;
+    while (!myfile.eof()) {
+      myfile >> str;
  
     }
  
