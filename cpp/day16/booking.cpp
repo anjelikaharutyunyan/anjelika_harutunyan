3bce2329615f43e315e53e2bf9eb16b5c3660bd4
Angelika <you@example.com>
2022-03-31 15:34:20 -0400

    added booking files

---
+++ cpp/day16/booking.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "booking.h"
+
+using namespace std;
+
+	struct agent{
+
+	string city;
+	string hotel_name;
+	int star;
+	int rooms;
+	string spa;
+	string gym;
+
+	};
+
+void agent (){
+
+cout << "Enter what you want to add: ";
+cout << "| 1-city | 2-hotel's name | 3-number of hotel's stars| 4-rooms | 5-spa | 6-gym |";
+
+string command = "add";
+int size = 6;
+string add[size];
+	while(command == "add"){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
