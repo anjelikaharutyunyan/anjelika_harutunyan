349b32f947dfa7e09e35f0eb788287656b3f7679
Angelika <you@example.com>
2022-03-31 16:32:33 -0400

    booking

--- cpp/day16/booking.cpp
+++ cpp/day16/booking.cpp
@@ -1,43 +1,61 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include "booking.h"
 
 using namespace std;
 
-	struct agent{
+int main(){
 
 	string city;
-	string hotel_name;
+	string hotelname;
 	int star;
-	int rooms;
-	string spa;
-	string gym;
-
-	};
-
-void agent (){
-
-cout << "Enter what you want to add: ";
-cout << "| 1-city | 2-hotel's name | 3-number of hotel's stars| 4-rooms | 5-spa | 6-gym |";
-
-string command = "add";
-int size = 6;
-string add[size];
-	while(command == "add"){
+	int singlroom;
+	int twinroom;
+	int tripleroom;
+	bool spa;
+	bool gym;
+	bool parking;
 
 
 
+	fstream booklist;
+	booklist.open("boking.txt");
+	string agent[9] = "";
+	
+	while(!booklist.eof()){
 
+	
+	cout << "Choose the city: ";
+	cin >> city;
 
+	cout << "Enter hotel name: ";
+	cin >> hotelname;
 
+	cout << "Enter star rate (1-5): ";
+	cin >> star;
 
+	cout << "Enter singl Room: ";
+	cin >> singlroom;
 
+	cout << "Enter twin Room: ";
+	cin >> twinroom;
 
+	cout << "Enter triple Room: ";
+	cin >> tripleroom;
+	
+	cout << "Enter existing of spa (0/1) : ";
+	cin >> spa;
+	
+	cout << "Enter existing of gym (0/1): ";
+	cin >> gym;
 
+	cout << "Enter existing of parking (0/1): ";
+	cin >> parking;
 
+	i}
 
+	booklist.close();
 
-
-
+/*	string booking = city + " " + to_string(hotelname) + " " + to_string(star) + " " + to_string(singlroom) + " " + to_string(twinroom) + " " + to_string(tripleroom) + " " + to_string(spa) + " " + to_string(gym) + " " + to_string(parking);
+*/
+return 0;
 }
