210f6edf7eb71572666cea6e37d131cf7a642792
Angelika <you@example.com>
2022-04-04 14:58:43 -0400

    added rectangle homework

--- cpp/day1/5_homework.cpp
+++ cpp/day1/5_homework.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 int main()
 {
-int edg, edg2;
-	cout << "Enter the 1st edg of rectangle ";
-	cin >> edg;
-	cout << "Enter the 2nd edg of rectangle ";
+
+int edg1, edg2;
+
+	cout << "Enter the first edg of rectangle ";
+	cin >> edg1;
+	cout << "Enter the second edg of rectangle ";
 	cin >> edg2;
-	 cout << "Area of Rectangle = " << edg*edg2 << endl;
-	cout << "Perimeter of Rectangle = " << (edg+edg2)*2;
+	cout << "Area of Rectangle = " << edg1 * edg2 << endl;
+	cout << "Perimeter of Rectangle = " << (edg1 + edg2) * 2;
 
 
 return 0;
