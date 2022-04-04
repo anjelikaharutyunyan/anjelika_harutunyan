bda557bc847e8283dbc47ecd3cc4ce32bf523d18
Angelika <you@example.com>
2022-04-04 15:02:14 -0400

    added factorial homework

--- cpp/day1/6_homework.cpp
+++ cpp/day1/6_homework.cpp
@@ -5,14 +5,17 @@ using namespace std;
 
 int main()
 {
-int number,count=1,faktorial=1;
-	cout << "Enter number";
+
+int number, count = 1, faktorial = 1;
+	cout << "Enter number ";
 	cin >> number;
-while (count <= number){
- faktorial = faktorial*count;
- count = count+1;
+
+	while (count <= number){
+	
+	 faktorial *= count;
+	 count++;
 }
-	 cout << "Factorial of Given Number is "<<faktorial<<endl;
+	 cout << "Factorial of Given Number is " << faktorial << endl;
 
 
 return 0;
