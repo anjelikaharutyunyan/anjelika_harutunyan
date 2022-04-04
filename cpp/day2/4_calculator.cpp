09a9a19c6330314210ab861edd2ddfad26fb9220
Angelika <you@example.com>
2022-04-04 15:22:08 -0400

    added calculator homework

--- cpp/day2/4_calculator.cpp
+++ cpp/day2/4_calculator.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int a,b;
-char x;
-        cout << "Enter number ";
-        cin >> a;
-        cout << "Enter operator ";
-        cin >> x;
-        cout << "Enter number ";
-        cin >> b;
-switch ( x ){
-        case '+': cout << a <<"+" << b << "=" << a+b; break;
-         case '-': cout << a << "-" << b << "=" << a-b;break;
-        case '*': cout << a << "*" << b << "=" << a*b;break;
-        case '/': cout << a << "/" << b << "=" << a/b;break;
+    int num1, num2;
+    char operato_r;
+    
+    cout << "Input first number ";
+    cin >> num1;
+ 
+    cout << "Enter operator ";
+    cin >> operato_r;
+ 
+    cout << "Enter number ";
+    cin >> num2;
 
-}
+	switch (operato_r){
+        	case '+':
+			 cout << num1 << " + " << num2 << " = " << num1 + num2;
+			 break;
+        	case '-': 
+			 cout << num1 << " - " << num2 << " = " << num1 - num2;
+			 break;
+        	case '*':
+			 cout << num1 << " * " << num2 << " = " << num1 * num2;
+			 break;
+       		case '/':
+			 cout << num1 << " / " << num2 << " = " << num1 / num2;
+			 break;
 
+	}
 
     return 0;
 }
