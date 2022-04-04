2858bcdc1c675304b4b8868912308bc39250c33d
Angelika <you@example.com>
2022-04-04 14:42:41 -0400

    added multiplication table homework

--- cpp/day1/2_homework.cpp
+++ cpp/day1/2_homework.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <math.h>
+
 using namespace std;
 
 int main()
 {
-  int x,i = 1;
-	cout << "mutqagrel tivy ";
-	cin >> x;
-	while(i <= 10){
-	cout << x << "*" << i << "=" << x*i <<endl;
+  int number, i = 1;
+	cout << "Enter a number for multiplication: ";
+	cin >> number;
+
+	while (i <= 10){
+	cout << number << "*" << i << "=" << number * i << endl;
 	i++;
 }
+
   return 0;
 }
 
