ff6c75c7b542459d53fee798a89ec7537e030d2e
Angelika <you@example.com>
2022-04-04 14:51:02 -0400

    added leap year homework

--- cpp/day1/3_homework.cpp
+++ cpp/day1/3_homework.cpp
@@ -4,18 +4,23 @@ using namespace std;
 
 int main()
 {
-int year, count=0;
-cin>>year;
-while ( count != 20 ){
+
+int year, count = 0;
+
+	cout << "Enter year: ";
+	cin >> year;
+
+	while (count != 20){
     
-if  (year % 4 == 0&& year % 100 != 0){
-         count = count + 1;
-        cout << year << "is leap year "<<endl;
-}
-        year++;
+	if  (year % 4 == 0 && year % 100 != 0){
+		count =  count + 1;
+		cout << year << " is leap year "<< endl;
+	}
+        	year++;
        
-}
+	}
 
 return 0;
+
 }
 
