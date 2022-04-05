c3649a46e726ca9e1de240f3ddd726c4aeed68cd
Angelika <you@example.com>
2022-04-05 10:33:17 -0400

    added homeworks day01

---
+++ cpp/day01/2_multiplication.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+  int number, i = 1;
+	cout << "Enter a number for multiplication: ";
+	cin >> number;
+
+	while (i <= 10){
+	cout << number << "*" << i << "=" << number * i << endl;
+	i++;
+}
+
+  return 0;
+}
+
