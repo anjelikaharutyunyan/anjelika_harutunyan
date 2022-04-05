d38af26f8618b228699e399951774529fa636b11
Angelika <you@example.com>
2022-04-05 12:32:39 -0400

    added even line matrix

--- cpp/day06/4_even_matrix.cpp
+++ cpp/day06/4_even_matrix.cpp
@@ -1,46 +1,50 @@
-   #include <iostream>
+#include <iostream>
 #include <time.h>
 #include <stdlib.h>
 using namespace std;
 
-int main()
-{
-    srand(time(0));
-const  int  m = 6;
-const int n = 3;
- 
-   int arr[m][n],j,i;
-   int d=0;
+int main(){
+srand(time(0));
+
+int  m, n;
+cout << "Rows :  ";
+cin >> m;
+cout << "Columns : ";
+cin >> n;
+
+const int row = m , column = n; 
+int arr[row][column];
+int temp = 0;
    
-   for( i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            arr[i][j] = 1 + rand() %5;
+   for(int i = 0; i < row; i++){
+        for(int j = 0; j < column; j++){
+            arr[i][j] = 1 + rand() % 5;
             cout << arr [i][j] << "  ";
         }
          cout << endl;
    }
-      for(int i = 0; i < m; i++){
-        int even = 0;
-        int count=0;
-        for(int j = 0; j < n; j++){
-          if(arr[i][j]%2==0 ) {
+      
+	for(int i = 0; i < row; i++){
+        int even = 0, count = 0;
+       
+        for(int j = 0; j < column; j++){
+          if(arr[i][j] % 2 == 0){
               count++;
-              if(count==j){
+              if(count == j){
                   even = arr[i][j];
               }
           }     
+
         }
-        if(count==n){
+        if(count == column){
               cout << i+1 << " line is even" << endl;
               break;
-              d = i-1;
-          }else
-        if( d == m-1 ){
-                  cout << "NO" << endl;
+              temp = i-1;
           }
-         d++;
+	else if( temp == row - 1 ){
+                  cout << "There is NO even line" << endl;
+          }
+         temp++;
    }
     return 0;
 }
-
-
