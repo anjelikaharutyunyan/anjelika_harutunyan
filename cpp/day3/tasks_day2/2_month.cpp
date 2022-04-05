b7c7b5b5f23aea9c87acf33806f3a26a2e4dc6d3
Angelika <you@example.com>
2022-04-05 02:31:48 -0400

    added

--- cpp/day3/tasks_day2/2_month.cpp
+++ cpp/day3/tasks_day2/2_month.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
-
 using namespace std;
  
-  int f ( int month){
- 	switch ( month ) {
-    case 1: case 3: case 7:case 8:case 10: cout << "Total number of days: 31 "; break;
-    case 4: case 6:case 9:case 11: cout << "Total number of days: 30 "; break;
-    case 2: cout << "Total number of days: 29 or 28 ";
-    break;
-  }
+void MonthDay(int month){
 
-    return 0;
+ 	switch (month){
+
+    		case 1:
+		case 3:
+		case 7:
+		case 8:
+		case 10:
+			 cout << "Total number of days: 31 ";
+			 break;
+    		case 4:
+		case 6:
+		case 9:
+		case 11:
+			 cout << "Total number of days: 30 ";
+			 break;
+    		case 2:
+			 cout << "Total number of days: 29 or 28 ";
+   			 break;
+  	}
 }
 
+int main() {
+	int month;
+	cout << "Enter month : ";
+	cin >> month;
 
+MonthDay(month);
 
-
-int main()
-{
-	return f(4);
+return 0;
 
 }
