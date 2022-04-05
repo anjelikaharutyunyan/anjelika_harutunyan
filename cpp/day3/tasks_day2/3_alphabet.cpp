100fc0caeb6f9d75da96ff40006ca582d27f69c3
Angelika <you@example.com>
2022-04-05 02:38:29 -0400

    added alphabet

--- cpp/day3/tasks_day2/3_alphabet.cpp
+++ cpp/day3/tasks_day2/3_alphabet.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 using namespace std;
 
-char f( char alphabet){
-        cout << "Inputed alphabet ";
-
-	if ( alphabet >= 'a' &&  alphabet <= 'z'  ||  alphabet >= 'A' && alphabet <= 'Z' ){
-
- switch ( alphabet ){
+void alphabet (char letter){
+     
+	if (letter >= 'a' &&  letter <= 'z'  ||  letter >= 'A' && letter <= 'Z'){
+		switch (letter){
        
-        case 'a': case 'A': case 'e': case 'E': case 'i': case 'I':
-        case 'o': case 'O':  cout << alphabet << " is a vowel "; break;
-        default : cout << alphabet << " is a consonant ";
-}
-}
-return 0;
+       			 case 'a':
+			 case 'A':
+			 case 'e':
+			 case 'E':
+			 case 'i':
+			 case 'I':
+			 case 'o':
+			 case 'O':
+				  cout << letter << " is a vowel ";
+				  break;
+       			 default :
+				  cout << letter << " is a consonant ";
+				  break;
+		}
+	}
+ 
+	else {
+		cout << letter << "is not character " << endl;
+	}
 }
 
 int main() {
+char letter;
+cout << "Enter an alphabet ";
+cin >> letter;
 
-return f ( 'g' );
+alphabet(letter);
+
+return 0;
 }
