71d077a2af59c8ed7a1ed248a338042050adecdd
Angelika <you@example.com>
2022-04-05 10:41:51 -0400

    added BB string

--- cpp/day06/1_BB.cpp
+++ cpp/day06/1_BB.cpp
@@ -2,21 +2,24 @@
 #include <string.h>
 using namespace std;
 
-int main()
-{
-     string str = "BBiig BBuble BBIG BBug BBin BB BBuble";
-    int count = 0, B = 0;
-    for (int i = 0; i <  str.length() ; i++){
-        if ( str[i] == 'B'){
-               count++;
-              
+int main(){
+
+string str;
+cout << "Enter the sentance : ";
+getline(cin,str);
+
+int count = 0, B = 0;
+	for (int i = 0; i <  str.length() ; i++){
+        	if ( str[i] == 'B'){
+               		count++;
         }
+
         if((str[i + 1] == ' ' || str[i + 1] == '\0') && count == 2){
            ++B;
            count = 0;
         }
     }
-        cout << B;
+        cout << "The number of words with 'B' are " << B;
     return 0;
 }
 
