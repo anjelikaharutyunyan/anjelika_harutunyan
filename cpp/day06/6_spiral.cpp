d7b18cc3d70a62dcd9aa09b440da33e9c084f300
Angelika <you@example.com>
2022-04-05 12:44:26 -0400

    added spiral matrix

--- cpp/day06/6_spiral.cpp
+++ cpp/day06/6_spiral.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    const int n = 5;
-    int m = 1;
-    int arr[n][n] = {0};
+int main(){
+int n;
+cout << "Enter array size: ";
+cin >> n;
+const int size = n;
+int m = 1;
+int arr[size][size] = {0};
     
-   /*if(n % 2 != 0){
-        arr[(n / 2)][(n / 2)] = (n * n);
-    }*/
-    
-    for(int i = 0; i < (n / 2); i++){
-        for(int j = i; j < (n - i); j++){
+for(int i = 0; i < (size / 2); i++){
+       for(int j = i; j < (size - i); j++){
             arr[i][j] = m;
             m++;
         }
-        for( int j = 1; j < (n - i - i); j++){
-            arr[(j + i)][(n - i) - 1] = m;
+        for( int j = 1; j < (size - i - i); j++){
+            arr[(j + i)][(size - i) - 1] = m;
             m++;
         }
-        for( int j = (n - 2) - i; j >= i; j--){
-            arr[(n - i) - 1][(j)] = m;
+        for( int j = (size - 2) - i; j >= i; j--){
+            arr[(size - i) - 1][(j)] = m;
             m++;
         }
-        for(int j = ((n - i) - 2); j > i; j--){
+        for(int j = ((size - i) - 2); j > i; j--){
             arr[j][i] = m;
             m++;
         }
-    }
-    for(int i = 0; i < n; i++){
-        for(int  j = 0; j < n - 1; j++){
+}
+
+for(int i = 0; i < size; i++){
+	for(int  j = 0; j < size - 1; j++){
             cout << arr[i][j] << " ";
         }
-        cout << arr[i][n - 1] << endl;
-    }
+        cout << arr[i][size - 1] << endl;
+}
     
     return 0;
 }
