9ed9b7ba6eaa12273f9593a7d9bcec46ff7e8dd3
Angelika <you@example.com>
2022-04-05 01:25:47 -0400

    added leap year

--- cpp/day3/tasks_day1/3_homework.cpp
+++ cpp/day3/tasks_day1/3_homework.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
-
 using namespace std;
 
-int f ( int year ){
-int  count=0;
-while ( count != 20 ){
-    
-if  ( year % 4 == 0 && year % 100 != 0 ){
-         count = count + 1;
-        cout << year << "is leap year " << endl;
-}
-        year++;
-       
-}
+void leap_year (int year){
 
-return 0;
+	int count = 0;
+
+	while (count != 20){
+    
+		if  ( year % 4 == 0 && year % 100 != 0 ){
+         		count++;
+        		cout << year << " is leap year " << endl;
+		}
+       			 year++;
+       
+	}
 }
 
 
 int main()
 {
 
-return f( 2020);
+int year;
+cout << "Enter year : ";
+cin >> year;
 
+leap_year(year);
+return 0;
 }
