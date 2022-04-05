b128031b02cea760fafc98b0669246d01c1d8c97
Angelika <you@example.com>
2022-04-05 11:07:11 -0400

    added 3rd homework

--- cpp/day06/3_array.cpp
+++ cpp/day06/3_array.cpp
@@ -3,22 +3,27 @@
 #include <ctime>
 #include <math.h>
 using namespace std;
+
 int main(){
-int arr[n];
-const int = 10;
-int j = 0, number = 0;
+    
+srand(time(0)); 
+int size, temp = 0, number = 0;
+cout << "Enter size : ";
+cin >> size;   
+int *arr = new int [size];
 int a, b;
-	for(int i = 0; j != n; i++){
-		a =  1 + random()%20;
-		b = 1 + random()%20;
-		number  = a*a + b*b;
-	if( number % 5 == 0){
-		arr[j] = number;
-		cout << arr[j] << endl;
-		j++ ;
+
+for(int i = 0; temp != size; i++){
+	a =  1 + random()%20;
+	b = 1 + random()%20;
+	number  = a*a + b*b;
+
+	if (number % 5 == 0){
+		arr[temp] = number;
+		cout << arr[temp] << " ";
+		temp++ ;
 	}	
 }
-
 
 return 0;
 }
