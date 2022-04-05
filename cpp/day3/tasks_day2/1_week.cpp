10e0f1708a8522c1726ada4608cacc62604b10a7
Angelika <you@example.com>
2022-04-05 02:20:36 -0400

    added week day

--- cpp/day3/tasks_day2/1_week.cpp
+++ cpp/day3/tasks_day2/1_week.cpp
@@ -1,22 +1,43 @@
-#include <iostream>
- 
+#include <iostream> 
 using namespace std;
-  int f ( int week ){
 
-switch ( week ) {
-       case 1: cout << "Monday\n "; break;
-       case 2:cout << "Tuesday\n "; break;
-       case 3:cout << " Wednesday \n"; break;
-       case 4:cout << "Thursday \n"; break;
-       case 5:cout << "Friday \n"; break;
-       case 6:cout << " Saturday\n"; break;
-       case 7:cout << "Sunday\n "; break;
-       default : cout <<" Enter number in range 1-7";
+void WeekDay (int day){
+
+	switch (day) {
+		case 1:
+			cout << "Monday\n ";
+			 break;
+		case 2:
+			cout << "Tuesday\n ";
+			 break;
+       		case 3:
+			cout << " Wednesday \n";
+			 break;
+       		case 4:
+			cout << "Thursday \n";
+			 break;
+       		case 5:
+			cout << "Friday \n";
+			 break;
+       		case 6:
+			cout << " Saturday\n";
+			 break;
+       		case 7:
+			cout << "Sunday\n ";
+			 break;
+       		default:
+			 cout << " Enter number in range 1-7 ";
 }
-  return 0;
+
 }
 
 int main (){
-	cout << " Week day is : ";
-   return  f(4);
+
+int day;
+cout << " Week day is : ";
+cin >> day;
+
+ WeekDay(day);
+
+return 0;
 }
