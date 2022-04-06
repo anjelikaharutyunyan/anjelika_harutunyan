479f7f858472de1e4fd7633f9777132fbfb93026
Angelika <you@example.com>
2022-04-06 02:09:41 -0400

    added my atoi

--- cpp/day08/atoi.cpp
+++ cpp/day08/atoi.cpp
@@ -2,19 +2,18 @@
 #include "atoi.h"
 using namespace std;
 
-int myatoi (char  *atoi) {
-	 
-	int result = 0;
+int myatoi(char *atoi) {
+int result = 0;
+for (int i = 0; atoi[i] != '\0'; i++){
+	if(atoi[i] >= 48 && atoi[i] <= 57){
+		result = result * 10 + (atoi[i] - '0');
 
-	for ( int i = 0;  atoi[i] != '\0'; i++){
-		if( atoi[i] >= 48 && atoi[i] <= 57){
-			result = result * 10 + (atoi[i] - '0');
+	} 
+	else{
+	break;
+	}
 
-	} else { 
-		break;
-	 }
- 	   
-}
-		return result;
+ 	}
 
+return result;
 }
--- cpp/day08/main.cpp
+++ cpp/day08/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "atoi.h"
 using namespace std;
+
 int main (int argc, char *argv[]){
 int sum = 0;
-
 if(argc == 3){
-sum = myatoi(argv[1]) + myatoi(argv[2]);
+	sum = myatoi(argv[1]) + myatoi(argv[2]);
+	cout << sum << endl  << endl;
 
-cout << sum << endl  << endl;
 }
 return 0;
 }
