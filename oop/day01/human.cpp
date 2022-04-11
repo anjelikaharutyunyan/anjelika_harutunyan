efbece491179200afc03f5abf3c0c05b2a72d038
Angelika <you@example.com>
2022-04-11 15:22:28 -0400

    added homework without .h file

---
+++ oop/day01/human.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+enum EyeColor{
+	Black,blue,grey,green,brown
+};
+enum HairColor {
+	white,ginger,black
+};
+enum Gender{
+	Male,Female
+};
+
+class Human{
+public:
+	Date birth_date;
+	EyeColor eye_color;
+	HairColor hair_color;
+	Gender gender;
+	int healthLevel;
+	char * Speak();
+	void Walk();
+	void See();
+	void Breathe();
+	bool Eat();
+	bool Drink();
+	int Sleep();
+
+private: 
+	void Think();	
+};
+
+class HomoSapience : Human {
+private: 
+	Address address;
+	string name;
+	string surname;
+	Style style;
+	Nationality nationality;
+	void work();
+	};
+
+class Programmer : HomoSapience {
+public:
+	int sallary;
+	bool TestCode();	
+	void WriteCode();
+	void Smoke();
+	void GetAngry();
+	void GetHappy();	
+	void GetTyred();
+};
+
+int main(){
+
+	
+
+
+return 0;
+}
